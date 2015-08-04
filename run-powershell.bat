15b69378865100833fc3ad612cc8f3f12998e735
boopathid <boopathid@users.noreply.github.com>
2015-08-04 12:24:24 -0700

    Create run-powershell.bat

---
+++ run-powershell.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+C:\Windows\syswow64\Windowspowershell\v1.0\powerShell.exe -NoProfile -Command "& {Start-Process powerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\Program Files (x86)\Boot.ps1""' -Verb RunAs}"
